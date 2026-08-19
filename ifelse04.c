66d32c77cafb7c39c874b71e7e996611397f7ebd
Suryansh <suryanshdev999@gmail.com>
2026-08-19 23:13:27 +0530

    Solved with if else instead of switch case

--- ifelse04.c
+++ ifelse04.c
@@ -3,15 +3,8 @@
 int main(){
     char ch;
     scanf("%c", &ch);
-    switch(ch){
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-            printf("The character is a Vowel");
-            break;
-        default:
-            printf("The character is a Consonant");
-    }
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        printf("Vowel");
+    else
+        printf("Consonant");
 }
