29f5d74979f3a2f7e3b5f23f99b5446e0fd51668
Suryansh <suryanshdev999@gmail.com>
2026-08-19 18:44:29 +0530

    C Programming Assignment Answer 9

---
+++ CPA9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+    float f;
+    double d;
+    char c;
+
+    printf("%d\n",(int)sizeof(i));
+    printf("%d\n",(int)sizeof(f));
+    printf("%d\n",(int)sizeof(d));
+    printf("%d",(int)sizeof(c));
+    
+}
