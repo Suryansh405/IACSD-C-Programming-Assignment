af61b4cb9ac0395e13d9a6538d4f92e3bfd4c915
Suryansh <suryanshdev999@gmail.com>
2026-08-21 16:34:54 +0530

    loops answer 7

--- loops07.c
+++ loops07.c
@@ -3,30 +3,34 @@
 #define PI 3.14
 
 int main(){
-    int operation;
+    int operation=0;
     float radius, length, breadth, base, height;
-    
-    printf("MENU\nChoose the operation:\n1. Area of Circle\n2. Area of Rectangle\n3. Area of Triangle\n");
-    scanf("%d", &operation);
-    
-    switch (operation)
-    {
-    case 1:
-        printf("Enter the radius of the circle: ");
-        scanf("%f", &radius);
-        printf("Area = %.2f", PI*pow(radius,2));
-        break;
-    case 2:
-        printf("Enter length and breadth of the rectangle: ");
-        scanf("%f %f", &length, &breadth);
-        printf("Area = %.2f", length*breadth);
-        break;
-    case 3:
-        printf("Enter base and height of the triangle: ");
-        scanf("%f %f", &base, &height);
-        printf("Area = %.2f", 0.5*base*height);
-        break;
-    default:
-        printf("Invalid Operation");
-    }    
+        
+    while(operation!=4){
+        printf("MENU\nChoose the operation:\n1. Area of Circle\n2. Area of Rectangle\n3. Area of Triangle\n4. EXIT\n");
+        scanf("%d", &operation);
+        switch (operation)
+        {
+        case 1:
+            printf("Enter the radius of the circle: ");
+            scanf("%f", &radius);
+            printf("Area = %.2f\n", PI*pow(radius,2));
+            break;
+        case 2:
+            printf("Enter length and breadth of the rectangle: ");
+            scanf("%f %f", &length, &breadth);
+            printf("Area = %.2f\n", length*breadth);
+            break;
+        case 3:
+            printf("Enter base and height of the triangle: ");
+            scanf("%f %f", &base, &height);
+            printf("Area = %.2f\n", 0.5f*base*height);
+            break;
+        case 4:
+            printf("THANK YOU");
+            break;
+        default:
+            printf("Invalid Operation");
+        }    
+    }
 }
